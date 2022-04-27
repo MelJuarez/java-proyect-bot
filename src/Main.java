153ea1a3e07ff06dc3e694e9c9e62fb7d525b5f7
MelJuarez <mnjuarez@scl.edu.gt>
2022-04-27 12:01:42 -0600

    Bienvenida de y primeras palabras de mi asistente
    Welcome from and first words from my assistant

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HOLA! soy  Téo\uD83C\uDF31, el asistente virtual del Café, te guiaré en tu orden el día de Hoy \n" +
+                "\n" +
+                "¿Qué te gustaría pedir?");
+    }
+}
